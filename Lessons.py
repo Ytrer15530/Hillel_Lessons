1b767d406b40122f9675c3fc7e2cf37272b3d056
Ytrer15530 <ilya15530@gmail.com>
2023-09-08 19:48:26 +0300

    Lesson 4 for loops 1/2

--- Lessons.py
+++ Lessons.py
@@ -98,3 +98,32 @@
 
 ########################
 # Lesson 4
+
+# for i in range(5):
+#     #print("Hello")
+#     print(i, end=" ")
+#
+# print()
+#
+# for i in range(2, 5):
+#     #print("Hello")
+#     print(i, end=" ")
+#
+# print()
+#
+# for i in range(1, 5, 2):
+#     #print("Hello")
+#     print(i, end=" ")
+#
+# print()
+#
+# start, end = 1, 10
+# for i in range(start, end + 1):
+#     print(i, end=" ")
+#
+# print()
+#
+# for i in range(end, start - 1, -1):
+#     print(i, end=" ")
+##########
+
